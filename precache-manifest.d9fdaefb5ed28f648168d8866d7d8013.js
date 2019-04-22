1cd4298a3d104898338136c1b7583cd6682c275d
Henry Myers <hmyers@landr.com>
2019-04-21 23:34:41 -0400

    Updates

---
+++ precache-manifest.d9fdaefb5ed28f648168d8866d7d8013.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8583e1a5a438cf8eec6ccc23e2f0ebd1",
+    "url": "/pool-of-thrones/static/media/logo.8583e1a5.png"
+  },
+  {
+    "revision": "5194255866ce3b2d26ad",
+    "url": "/pool-of-thrones/static/js/runtime~main.947ce9ff.js"
+  },
+  {
+    "revision": "e11c9adc30f335d0240d",
+    "url": "/pool-of-thrones/static/js/main.d2f66cb7.chunk.js"
+  },
+  {
+    "revision": "59c8c4ef1a695b9f41e0",
+    "url": "/pool-of-thrones/static/js/2.26c8a8d0.chunk.js"
+  },
+  {
+    "revision": "e11c9adc30f335d0240d",
+    "url": "/pool-of-thrones/static/css/main.5eefa465.chunk.css"
+  },
+  {
+    "revision": "59c8c4ef1a695b9f41e0",
+    "url": "/pool-of-thrones/static/css/2.b653801d.chunk.css"
+  },
+  {
+    "revision": "672d926d7089372c6efd20d44a674e10",
+    "url": "/pool-of-thrones/index.html"
+  }
+];
